9254f173425c091363ca122ea849f8b1c1eb23f1
Naotsun <51815450+Naotsun19B@users.noreply.github.com>
2020-12-02 00:54:28 +0900

    Add launch game command

---
+++ UprojectShortcuts/Commands/LaunchGameCommand.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.ComponentModel.Design;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace UprojectShortcuts.Commands
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class LaunchGameCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("c5b5cf03-742f-40ad-b231-96dcbb0606ec");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage Package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LaunchGameCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="Package">Owner package, not null.</param>
+        /// <param name="CommandService">Command service to add command to, not null.</param>
+        private LaunchGameCommand(AsyncPackage Package, OleMenuCommandService CommandService)
+        {
+            this.Package = Package ?? throw new ArgumentNullException(nameof(Package));
+            CommandService = CommandService ?? throw new ArgumentNullException(nameof(CommandService));
+
+            CommandID MenuCommandID = new CommandID(CommandSet, CommandId);
+            MenuCommand MenuItem = new MenuCommand(this.Execute, MenuCommandID);
+            CommandService.AddCommand(MenuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static LaunchGameCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.Package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="Package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage Package)
+        {
+            // Switch to the main thread - the call to AddCommand in ShortcutCommands's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(Package.DisposalToken);
+
+            OleMenuCommandService CommandService = await Package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+            Instance = new LaunchGameCommand(Package, CommandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="Sender">Event sender.</param>
+        /// <param name="Args">Event args.</param>
+        private void Execute(object Sender, EventArgs Args)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            string FilePath = FunctionLibrary.GetCurrentUprojectFilePath();
+            FunctionLibrary.InvokeUprojectCommand(
+                UprojectCommandType.LaunchGame, 
+                FilePath
+            );
+        }
+    }
+}
---
+++ UprojectShortcuts/FunctionLibrary.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
+
+namespace UprojectShortcuts
+{
+    /// <summary>
+    /// Define commands that can be executed from the right-click menu of the .uproject file.
+    /// </summary>
+    enum UprojectCommandType
+    {
+        LaunchGame,
+        GenerateVisualStudioProjectFiles,
+        SwitchUnrealEngineVersion,
+    }
+
+    /// <summary>
+    /// A class that defines common functions used by this extension.
+    /// </summary>
+    class FunctionLibrary
+    {
+        /// <summary>
+        /// Unreal Engine project file extension.
+        /// </summary>
+        public static readonly string UprojectExtension = "uproject";
+
+        /// <summary>
+        /// Get the path of a .uproject file that is in the same hierarchy as the currently open .sln file.
+        /// Returns string.Empty if no .sln file is currently open.
+        /// </summary>
+        public static string GetCurrentUprojectFilePath()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            IVsSolution Solution = (IVsSolution)Package.GetGlobalService(typeof(IVsSolution));
+
+            if (Solution.GetSolutionInfo(out string SolutionDirectory, out _, out _) == VSConstants.S_OK)
+            {
+                string[] Files = Directory.GetFiles(
+                    SolutionDirectory, 
+                    "*." + UprojectExtension, 
+                    SearchOption.TopDirectoryOnly
+                );
+
+                if (Files.Length == 1)
+                {
+                    return Files[0];
+                }
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Execute by specifying the command type and the path of the .uproject file.
+        /// </summary>
+        /// <param name="CommandType">Type of command to execute.</param>
+        /// <param name="ProjectFilePath">Full file path of the target .uproject file.</param>
+        public static void InvokeUprojectCommand(UprojectCommandType CommandType, string ProjectFilePath)
+        {
+
+        }
+    }
+}
